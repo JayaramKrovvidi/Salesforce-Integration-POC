4573d86393c6e10faa8efd9f822ef9300cbc249c
Nandhini R <v-nandhini.r@turvo.com>
2021-02-26 09:36:37 +0530

    adding data after modification to csv file

--- src/main/java/com/integration/poc/services/impl/HeaderMapperResultProcessorImpl.java
+++ src/main/java/com/integration/poc/services/impl/HeaderMapperResultProcessorImpl.java
@@ -61,7 +61,6 @@ public class HeaderMapperResultProcessorImpl implements IResultProcessor {
 		}
 //	 case 1 2
 		for(String i:header) {
-			//System.out.println(i);
 			if(mappers.containsKey(i)) {
 			   mappers.get(i).put("visited","true");
 			   headerClone.set(header.indexOf(i),mappers.get(i).get("value"));
@@ -92,14 +91,16 @@ public class HeaderMapperResultProcessorImpl implements IResultProcessor {
 			}
 		}
 		
-		
+		String newHeader=String.join(",",headerClone);
 		List<String> newRows = new ArrayList<>();
 //		newRows.add(
 //				"id,vehicle_type,vin,notes,account.id,license.number,license.country,license.state,asset.number,make,model,size,class,color,weight,status,sensor.provider,sensor.deviceId,sensor.customId");
-
-		for (int i = 1; i < rows.length; i++) {
-			newRows.add(rows[i]);
+        newRows.add(newHeader);
+		for (int i = 1; i < contentClone.size(); i++) {
+			String newContent=String.join(",", contentClone.get(i));
+			newRows.add(newContent);
 		}
+		
 		String fileName = "asset_" + System.currentTimeMillis() + ".csv";
 		String filePath = "D:\\Salesforce POC\\"+ fileName;
 		FileWriter fileWriter = null;
