26d8d9be2487548475469c06d6c25bcea933d328
Mr-Akk <akm99spl@gmail.com>
2021-03-12 17:57:45 +0530

    connection made and adapter is working.Getting datas from api for connection

--- src/main/java/com/integration/poc/PocApplication.java
+++ src/main/java/com/integration/poc/PocApplication.java
@@ -15,24 +15,5 @@ public class PocApplication {
     SpringApplication.run(PocApplication.class, args);
   }
   
-  
-  //made to check if the connection is working.
-  //it is working
-  @PostConstruct
-  public void check() {
- 
-    try{  
-    Class.forName("com.mysql.cj.jdbc.Driver");  
-    Connection con=DriverManager.getConnection(  
-    "jdbc:mysql://localhost:3306/checkdb","root","root");  
-    Statement stmt=con.createStatement();  
-    System.out.println("connection made");
-    ResultSet rs=stmt.executeQuery("select * from checktable");  
-    
-    while(rs.next())  
-    System.out.println(rs.getInt(1)+"  "+rs.getString(2));  
-    con.close();  
-    }catch(Exception e){ System.out.println(e);}  
-    }  
   }
 
--- src/main/java/com/integration/poc/services/impl/DatabaseExecutorImpl.java
+++ src/main/java/com/integration/poc/services/impl/DatabaseExecutorImpl.java
@@ -4,44 +4,67 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Iterator;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Service;
 import com.integration.poc.dtos.internal.ApiRequestConfig;
+import com.integration.poc.dtos.internal.NameValuePair;
 import com.integration.poc.services.IApiExecutor;
 
+@Service
 public class DatabaseExecutorImpl implements IApiExecutor {
+
+  private static String USERNAME;
+  private static String PASSWORD;
+  private static String DRIVER;
+  private static String QUERY;
+  private static String DBURL;
 
   @Override
   public String executeApi(ApiRequestConfig apiRequest) {
-    System.out.println("database impl called");
+    extractData(apiRequest);
     switch (apiRequest.getMethodType()) {
       case "SELECT":
         return executeSelect(apiRequest);
       default:
         return null;
-    }
-    
+    } 
+  }
+  
+  private String executeSelect(ApiRequestConfig apiRequest) {   
+    createConnection();
+    return null;
+  }
+  
+  private void extractData(ApiRequestConfig apiRequest){
+    for(NameValuePair eachPair : apiRequest.getRequestParams()) {
+      if(eachPair.getName().equals("username")) {
+        USERNAME = (String) eachPair.getValue();
+      }
+      else if (eachPair.getName().equals("password")) {
+        PASSWORD = (String) eachPair.getValue();
+      }
+      else if (eachPair.getName().equals("driver")) {
+        DRIVER = (String) eachPair.getValue();
+      }
+      else if (eachPair.getName().equals("dburl")) {
+        DBURL = (String) eachPair.getValue();
+      }
+    }  
+    QUERY = (String) apiRequest.getRequestBody();
   }
   
   private void createConnection() {
     
     try{  
-      Class.forName("com.mysql.cj.jdbc.Driver");  
-      Connection con=DriverManager.getConnection(  
-      "jdbc:mysql://localhost:3306/checkdb","root","root");  
+      Class.forName(DRIVER);  
+      Connection con=DriverManager.getConnection(DBURL,USERNAME,PASSWORD);  
       Statement stmt=con.createStatement();  
-      System.out.println("connection made");
-      ResultSet rs=stmt.executeQuery("select * from checktable");  
-      
+      ResultSet rs=stmt.executeQuery(QUERY);    
       while(rs.next())  
       System.out.println(rs.getInt(1)+"  "+rs.getString(2));  
       con.close();  
       }catch(Exception e){ System.out.println(e);}  
       }  
-  
-
-  private String executeSelect(ApiRequestConfig apiRequest) {
-    
-    createConnection();
-    return null;
-  }
 
 }
